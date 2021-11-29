d64f4b96b582b25388a360b91dd287a5514e376f
Patrick Lin <apocalypsecalculator@gmail.com>
2021-11-28 21:26:53 -0500

    edit settings command

---
+++ commands/editsettings.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const arrayHelpers = require('../lib/array');
+
+exports.name = 'editsettings';
+exports.helpArgs = '[normal or noping or none]';
+exports.helpText = 'Settings for edited announcements forwarding';
+exports.permReq = "manage server";
+exports.verify = function (msg) {
+    return msg.member.permissions.has('MANAGE_GUILD');
+}
+
+exports.execute = function (msg, args, client) {
+    if (args[0] == "normal" || args[0] == "noping" || args[0] == "none") {
+        let rawdata = fs.readFileSync('./data/data.json');
+        let parsed = JSON.parse(rawdata);
+        let gpos = arrayHelpers.getElementByProperty(parsed.push, "guildid", msg.guildId);
+        if (gpos == -1) {
+            msg.channel.send('Announcements forwarding is not set up');
+        }
+        else {
+            parsed.push[gpos].editsettings = args[0];
+            let newraw = JSON.stringify(parsed);
+            fs.writeFileSync('./data/data.json', newraw);
+            msg.channel.send(`Set edited announcements forwarding settings to ${args[0]}`);
+        }
+    }
+    else {
+        msg.channel.send('Incorrect or missing arguments, refer to help arguments');
+    }
+}
